68ea881f3cd468725d7232bbf7c033fd8664ae0f
elliotreed <elliot@elliotreed.net>
2026-03-08 12:31:32 -0600

    vps deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,52 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import path from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Back Talk',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: "Backtalk",
+            social: [{ icon: "github", label: "GitHub", href: "https://github.com/withastro/starlight" }],
+            sidebar: [
+                {
+                    label: "Guides",
+                    autogenerate: { directory: "guides" },
+                },
+                {
+                    label: "Reference",
+                    items: [
+                        {
+                            label: "abstracts",
+                            autogenerate: { directory: "reference/abstracts" }
+                        }
+                    ]
+                    ,
+                },
+            ],
+            // customCss: ["./src/styles/index.scss"],
+        }),
+    ],
+    site: "https://docs.backtalk.elliotreed.dev",
+    vite: {
+        resolve: {
+            alias: {
+                "~": path.resolve("./src"),
+            },
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    loadPaths: [path.resolve("./src/styles")]
+                },
+            }
+        },
+        server: {
+            allowedHosts: ["backtalk.local"],
+            // These settings don't matter, the npm dev script sets these values via flags
+            host: "backtalk.local",
+            port: 5001,
+        },
+    },
 });
--- src/content/docs/reference/base/elements/table.md
+++
@@ -1,6 +0,0 @@
----
-title: "table.scss"
-description: "Base style for table elements"
----
-
-These are the table styles, imported by base.
